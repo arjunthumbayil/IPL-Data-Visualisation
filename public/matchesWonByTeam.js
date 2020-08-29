a0dd75d4903b43527b562b5e7e5b4d6a07049ac9
arjunthumbayil <40037640+arjunthumbayil@users.noreply.github.com>
2020-08-29 07:03:28 +0530

    Add files via upload

---
+++ public/matchesWonByTeam.js
@@ -0,0 +1,14 @@
+function matchesWonByTeam(matches) {
+  const result = {};
+  for (let match of matches) {
+    const winner = match.winner;
+    if (result[winner]) {
+      result[winner] += 1;
+    } else {
+      result[winner] = 1;
+    }
+  }
+  return result;
+}
+
+module.exports = matchesWonByTeam;
